589c587989d4302813780e4f33278578df8c6b0c
Robert <robert.pustulka@gmail.com>
2015-07-21 08:20:23 +0200

    Reverted EventManagerTrait as a stub file.

---
+++ Event/EventManagerTrait.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://cakephp.org CakePHP(tm) Project
+ * @since         3.0.0
+ * @license       http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+namespace Cake\Event;
+
+/**
+ * Provides the event manager interface features for usage in classes that require it.
+ *
+ * @deprecated 3.0.10 Use EventDispatcherTrait instead.
+ */
+trait EventManagerTrait
+{
+
+    use EventDispatcherTrait;
+}
